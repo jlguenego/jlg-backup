e6e282cdda297e2f7f008c27f4d57c0d9ae6daff
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-11-01 16:32:31 +0100

    added home page

--- client/src/app/layout/layout.module.ts
+++ client/src/app/layout/layout.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BodyComponent } from './body/body.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, BodyComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [HeaderComponent, FooterComponent, BodyComponent],
 })
 export class LayoutModule {}
