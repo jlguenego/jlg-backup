af44f58dfeafb06f1ed25f57c788f2f90bc5a41d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-11-05 16:37:30 +0100

    remove components

--- client/src/app/widget/widget.module.ts
+++ client/src/app/widget/widget.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RemoteFormComponent } from './remote-form/remote-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LocalFormComponent } from './local-form/local-form.component';
-import { IntervalFormComponent } from './interval-form/interval-form.component';
 import { OptionsFormComponent } from './options-form/options-form.component';
 
 @NgModule({
-  declarations: [RemoteFormComponent, LocalFormComponent, IntervalFormComponent, OptionsFormComponent],
+  declarations: [OptionsFormComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [RemoteFormComponent, LocalFormComponent, IntervalFormComponent, OptionsFormComponent],
+  exports: [OptionsFormComponent],
 })
 export class WidgetModule {}
