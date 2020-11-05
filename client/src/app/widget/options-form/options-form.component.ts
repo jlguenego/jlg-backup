cff1d8abbd7f1b0816ff97bc7003f5096a884f96
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-11-05 16:35:42 +0100

    refactored form

---
+++ client/src/app/widget/options-form/options-form.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { BackupInfo } from 'src/app/interfaces/backup-info';
+import { BackupService } from 'src/app/services/backup.service';
+import { BackupOptions } from '../../../../../src/interfaces';
+
+@Component({
+  selector: 'app-options-form',
+  templateUrl: './options-form.component.html',
+  styleUrls: ['./options-form.component.scss'],
+})
+export class OptionsFormComponent implements OnInit {
+  f = new FormGroup({
+    remote: new FormControl(''),
+    local: new FormControl(''),
+    intervalInSecond: new FormControl(''),
+  });
+
+  backupInfo: BackupInfo;
+
+  constructor(private backupService: BackupService) {
+    this.backupService.backupInfo$.subscribe((backupInfo) => {
+      this.backupInfo = backupInfo;
+      this.reset();
+    });
+  }
+
+  ngOnInit(): void {}
+
+  reset(): void {
+    this.f.setValue({
+      remote: this.backupInfo.options.remote ?? '',
+      local: this.backupInfo.options.local ?? '',
+      intervalInSecond: this.backupInfo.options.intervalInSecond ?? '',
+    });
+  }
+
+  submit(): void {
+    console.log('submit');
+    this.backupService.update(this.f.value as BackupOptions);
+  }
+}
--- client/src/app/widget/widget.module.ts
+++ client/src/app/widget/widget.module.ts
@@ -4,10 +4,11 @@ import { RemoteFormComponent } from './remote-form/remote-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LocalFormComponent } from './local-form/local-form.component';
 import { IntervalFormComponent } from './interval-form/interval-form.component';
+import { OptionsFormComponent } from './options-form/options-form.component';
 
 @NgModule({
-  declarations: [RemoteFormComponent, LocalFormComponent, IntervalFormComponent],
+  declarations: [RemoteFormComponent, LocalFormComponent, IntervalFormComponent, OptionsFormComponent],
   imports: [CommonModule, ReactiveFormsModule],
-  exports: [RemoteFormComponent, LocalFormComponent, IntervalFormComponent],
+  exports: [RemoteFormComponent, LocalFormComponent, IntervalFormComponent, OptionsFormComponent],
 })
 export class WidgetModule {}
