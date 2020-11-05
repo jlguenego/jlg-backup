05f8ad470baa4a42d8b1aee8ba69bd0035d048ae
Jean-Louis GUENEGO <jlguenego@gmail.com>
2020-11-05 17:34:00 +0100

    added fontawesome module

--- client/src/app/widget/widget.module.ts
+++ client/src/app/widget/widget.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { OptionsFormComponent } from './options-form/options-form.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [OptionsFormComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FontAwesomeModule],
   exports: [OptionsFormComponent],
 })
 export class WidgetModule {}
