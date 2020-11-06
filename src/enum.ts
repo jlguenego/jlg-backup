0dd87cc692d566f991bde83e106c89c2e9c8b807
Backuper <please@enjoy-jlg-backup.com>
2020-11-06 17:45:23 +0100

    backup

--- src/enum.ts
+++ src/enum.ts
@@ -1,7 +1,7 @@
 export enum REMOTE {
   INIT = "Init...",
   NOT_SET = "Remote not set.",
-  NOT_EXISTING_DIR = "Local directory not existing.",
+  NOT_EXISTING_DIR = "Remote directory not existing.",
   OK = "Local directory set.",
 }
 
